1486531b2cc922381aa25ce07af3faf89acdc4a3
PoliakPavel <pashkapol27@gmail.com>
2022-08-10 13:09:03 +0300

    link (name) modify html

--- my_site/news/urls.py
+++ my_site/news/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<int:category_id>/', get_category)
+    path('category/<int:category_id>/', get_category, name='category')
     ]
