4070afd5ddea98942caf9794afece0fd248afe17
PoliakPavel <pashkapol27@gmail.com>
2022-08-10 12:51:19 +0300

    add page category/pk

--- my_site/news/urls.py
+++ my_site/news/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('category/<int:category_id>/', get_category)
     ]
--- my_site/news/views.py
+++ my_site/news/views.py
@@ -12,4 +12,9 @@ def index(request):
     }
     return render(request, 'news/index.html', context)
 
-# Create your views here.
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
